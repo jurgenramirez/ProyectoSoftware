26b9e09967259e57a6db9b66750a9a19a4000b90
Fernando Paz <fernandopaz1995@gmail.com>
2020-12-19 23:32:39 -0600

    AgregarSubastaxBugFix

--- src/app/subasta/subasta.component.ts
+++ src/app/subasta/subasta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-subasta',
@@ -14,6 +15,11 @@ export class SubastaComponent implements OnInit {
   user :string;
 
   categorias: any[] = [];
+
+  imageError: string;
+  isImageSaved: boolean;
+  cardImageBase64: string;
+  extension:string;
 
   constructor(private rutaActiva: ActivatedRoute,private http: HttpClient) { }
 
@@ -43,8 +49,7 @@ export class SubastaComponent implements OnInit {
     console.log(this.user);   
     console.log(this.sign.value.pi);
     console.log(this.sign.value.fe);
-    
-
+    console.log(this.cardImageBase64);
     
     
     this.http.post('http://34.66.195.21:5001/subasta',
@@ -55,7 +60,8 @@ export class SubastaComponent implements OnInit {
       precioVenta: this.sign.value.pv,
       idUsuario: this.user,
       precioInicial: this.sign.value.pi,
-      fechaFinal: this.sign.value.fe
+      fechaFinal: this.sign.value.fe,
+      foto: this.cardImageBase64
       
 
     }).subscribe((response) => {
@@ -65,6 +71,62 @@ export class SubastaComponent implements OnInit {
 
     
     
+
+  }
+
+  fileChangeEvent(fileInput: any) {
+
+    this.imageError = null;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+        // Size Filter Bytes
+        const max_size = 20971520;
+        const allowed_types = ['image/png', 'image/jpeg'];
+        const max_height = 15200;
+        const max_width = 25600;
+
+        if (fileInput.target.files[0].size > max_size) {
+            this.imageError =
+                'Maximum size allowed is ' + max_size / 1000 + 'Mb';
+
+            return false;
+        }
+
+        if (!_.includes(allowed_types, fileInput.target.files[0].type)) {
+            this.imageError = 'Only Images are allowed ( JPG | PNG )';
+            return false;
+        }
+        this.extension = fileInput.target.files[0].type;
+      
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            const image = new Image();
+            image.src = e.target.result;
+            image.onload = rs => {
+                const img_height = rs.currentTarget['height'];
+                const img_width = rs.currentTarget['width'];
+
+                console.log(img_height, img_width);
+
+
+                if (img_height > max_height && img_width > max_width) {
+                    this.imageError =
+                        'Maximum dimentions allowed ' +
+                        max_height +
+                        '*' +
+                        max_width +
+                        'px';
+                    return false;
+                } else {
+                    const imgBase64Path = e.target.result;
+                    this.cardImageBase64 = imgBase64Path;
+                    this.isImageSaved = true;
+                    // this.previewImagePath = imgBase64Path;
+                }
+            };
+        };
+
+        reader.readAsDataURL(fileInput.target.files[0]);
+    }
 
   }
 
